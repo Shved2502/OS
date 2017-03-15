4d08894291fceae453f65e8adee211a95ce05ee9
Shved2502 <vad.shvedov@mail.ru>
2017-03-15 20:54:25 +0300

    Update Task_3.c

--- Task_3.c
+++ Task_3.c
@@ -6,31 +6,31 @@
 #include <sys/shm.h>
 #include <string.h>
 
+int count = 0;
 int graph[][2] = {
-    {0, 1},
-    {0, 2},
-    {0, 3},
-    {1, 3},
-    {1, 2},
-    {2, 4},
-    {3, 2},
-    {4, 5},
-    {4, 6},
-    {5, 7},
-    {5, 8},
-    {6, 9},
-    {7, 8},
-    {8, 9}
+        {0, 1},
+        {0, 2},
+        {0, 3},
+        {1, 4},
+        {1, 5},
+        {2, 6},
+        {3, 7},
+        {5, 6},
+        {4, 7},
 };
 
 void run(int top, int *visit) {
     visit[top] = 1;
-    for (int i = 0; i < 14; ++i) {
+    for (int i = 0; i < 9; ++i) {
         if ((graph[i][0] == top) && (visit[graph[i][1]] == 0)) {
             visit[graph[i][1]] = 1;
             pid_t pid = fork();
             if (pid > 0) {
+                count++;
                 printf("Parent process: pid %i, creating process pid %i, %i-%i \n", getpid(), pid, top, graph[i][1]);
+                if (count == 7){
+                   printf("Sum = %i", count);
+                }
             } else {
                 run(graph[i][1], visit);
             }
@@ -45,14 +45,14 @@ int main() {
     int shared_mem_id;
     int *visit;
     if ((shared_mem_id = shmget(1, 6 * sizeof(int), IPC_CREAT | 0666)) < 0) {
-        fprintf(stderr, "Error while allocating shared memory.");
+        fprintf(stderr, "Error memory");
         return 1;
     }
-    if ((visited = (int *) shmat(shared_mem_id, NULL, 0)) == (int *) -1) {
-        fprintf(stderr, "Error while attaching shared memory.");
+    if ((visit = (int *) shmat(shared_mem_id, NULL, 0)) == (int *) -1) {
+        fprintf(stderr, "Error memory");
         return 1;
     }
-    memset(visit, 0, 10 * sizeof(int));
+    memset(visit, 0, 8 * sizeof(int));
     run(0, visit);
     return 0;
 }
